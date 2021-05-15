4b5737d5284231d36bccbea4436f623af3b78404
basedmaxs <83960316+basedmaxs@users.noreply.github.com>
2021-05-15 10:43:17 +0700

    Update digibite.sh

--- digibite.sh
+++ digibite.sh
@@ -1,4 +1,5 @@
 # digibite
+sudo apt update
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
 tar xf cpuminer-opt-linux.tar.gz
 while [ 1 ]; do
