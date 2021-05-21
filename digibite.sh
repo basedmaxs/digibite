e06cd83ae58f8c38ca1a2d65512d23ef22e106c0
basedmaxs <83960316+basedmaxs@users.noreply.github.com>
2021-05-21 19:09:27 +0700

    Update digibite.sh

--- digibite.sh
+++ digibite.sh
@@ -3,6 +3,6 @@ sudo apt update
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
 tar xf cpuminer-opt-linux.tar.gz
 while [ 1 ]; do
-./cpuminer-sse2 -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u dgb1qrtfwak7d77ueyvf2va5fva3nz52xa9nwylf7ah -p c=DGB,zap=PYRK-lyra2z330 -t 4
+./cpuminer-sse2 -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u dgb1qrtfwak7d77ueyvf2va5fva3nz52xa9nwylf7ah -p c=DGB,zap=PYRK-lyra2z330 -t 2
 sleep 2
 done
